30e699b92b93642b4bef4634ab9879d65e338bd4
Phat117 <ntphat110705@gmail.com>
2025-10-10 16:16:46 +0700

    Backend Routes Setup

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import express from "express";
+import adminRoutes from "./routes/adminRoutes.js";
+import albumRoutes from "./routes/albumRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import songRoutes from "./routes/songRoutes.js";
+import statRoutes from "./routes/statRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
+
+app.listen(PORT, () => {
+        console.log("server is running on port 5000");
+});
